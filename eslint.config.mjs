a4ef023bc18cd4055885453bfc01d184849addea
Yusunkim4448 <156165117+Yusunkim4448@users.noreply.github.com>
2024-09-21 17:05:27 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,18 +3,10 @@ import { defineConfig } from 'eslint-define-config';
 export default defineConfig({
   languageOptions: {
     globals: {
-      browser: true,
-      es2021: true,
-    },
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
+      structuredClone: 'readonly',
     },
   },
   rules: {
-    'no-unused-vars': 'warn',
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'eqeqeq': ['error', 'always'],
+    'no-unused-vars': 'error',
   },
 });
