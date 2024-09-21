dcdc28d7789773a50df27b3a5926d791af51c0a9
Yusunkim4448 <156165117+Yusunkim4448@users.noreply.github.com>
2024-09-21 17:11:24 -0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'eslint-define-config';
 export default defineConfig({
   languageOptions: {
     globals: {
-      structuredClone: 'readonly',
+      structuredClone: 'readonly', // Define structuredClone as a read-only global
+    },
+    parserOptions: {
+      ecmaVersion: 2021, // Specify the ECMAScript version
     },
   },
   rules: {
     'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
   },
 });
+//changes
