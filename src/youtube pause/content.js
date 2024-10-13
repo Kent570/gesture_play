bb40ded9af37bfffdc29147ae386a3704ae28616
Tai-Chieh Feng <126645223+DS0919@users.noreply.github.com>
2024-10-13 19:06:46 -0400

    change yt

--- src/youtube pause/content.js
+++ src/youtube pause/content.js
@@ -15,8 +15,17 @@ document.addEventListener('keydown', function(event) {
         if (event.key === 'b') {
             video.currentTime -= 10; // time-10
         }
-        if (event.key === 'r') {
-            video.currentTime =0; // start from the beginning
+        if (event.key === 'n') {
+            let nextButton = document.querySelector('.ytp-next-button, .button-nfnext, .skip__button, .next-episode-button, .next-up-card');
+            if (nextButton) {
+                nextButton.click(); // to next video
+            }
+        }
+        if (event.key === 'v') {
+            let prevButton = document.querySelector('.ytp-prev-button, .button-nfprev, .prev-button, .previous-episode-button');
+            if (prevButton) {
+                prevButton.click(); // to previous video
+            }
         }
         if (event.key === 'l') { // fullscreen
             if (!document.fullscreenElement) {
@@ -25,17 +34,8 @@ document.addEventListener('keydown', function(event) {
                 document.exitFullscreen();
             }
         }
-        if (event.key === 'n') {
-            let nextButton = document.querySelector('.ytp-next-button, .button-nfnext');
-            if (nextButton) {
-                nextButton.click(); // to next video
-            }
-        }
-        if (event.key === 'v') {
-            let prevButton = document.querySelector('.ytp-prev-button, .button-nfprev');
-            if (prevButton) {
-                prevButton.click(); // to previous video
-            }
+        if (event.key === 'r') {
+            video.currentTime =0; // start from the beginning
         }
         if (event.key === 'w') {
             let subButton = document.querySelector('.ytp-subtitles-button, .button-nfsubtitles');
