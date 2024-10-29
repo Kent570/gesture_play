77cbeaa8fe9b038504d82467b5038a582ac36482
Tai-Chieh Feng <126645223+DS0919@users.noreply.github.com>
2024-10-29 12:16:38 -0400

    change smooth

--- src/combine_all_test/content.js
+++ src/combine_all_test/content.js
@@ -1,33 +1,43 @@
 let videoControlActive = true; // flag to track if video control is active
 
+var numb = 0;
+
 function video_change() {
     let video = document.querySelector('video');
 
     if (isGestureMode && video) { // only allow video control when active
-        if (gesture_var == 1) { // play/pause
+        if (gesture_var == 1 && numb != 1) { // play/pause
             if (video.paused) {
                 video.play();
             } else {
                 video.pause();
             }
+            numb = 1;
         }
-        if (gesture_var == 3) {
-            video.currentTime += (10/3); // time+10
+        if (gesture_var == 3 && numb != 3) {
+            video.currentTime += 10; // time+10
+            numb = 3;
         }
-        if (gesture_var == 2) {
-            video.currentTime -= (10/3); // time-10
+        if (gesture_var == 2 && numb != 2) {
+            video.currentTime -= 10; // time-10
+            numb = 2;
         }
-        if (gesture_var == 5) {
+        if (gesture_var == 5 && numb != 5) {
             let nextButton = document.querySelector('.ytp-next-button, .button-nfnext, .skip__button, .next-episode-button, .next-up-card, .player-controls__next');
             if (nextButton) {
                 nextButton.click(); // to next video
             }
+            numb = 5;
         }
-        if (gesture_var == 4) {
+        if (gesture_var == 4 && numb != 4) {
             let prevButton = document.querySelector('.ytp-prev-button, .button-nfprev, .prev-button, .previous-episode-button, .player-controls__previous');
             if (prevButton) {
                 prevButton.click(); // to previous video
             }
+            numb = 4;
+        }
+        else if(gesture_var == 0||gesture_var == 6||gesture_var == 7){
+            numb = 0;
         }
     }
 }
